33a7510c1225f0f4beaf74d0d694e062cbf1a62d
JULIO FERNANDES DO COUTO <gotinhajulio@gmail.com>
2023-01-31 20:54:59 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const botaoTrailler = document.querySelector('.botao-trailler');
+const botaoFecharModal = document.querySelector('.fechar-modal');
+const video = document.getElementById('video');
+const modal = document.querySelector('.modal');
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle('aberto');
+}
+
+botaoTrailler.addEventListener('click', () => {
+    alternarModal();
+video.setAttribute('src', linkDoVideo);
+})
+
+botaoFecharModal.addEventListener('click', () => {
+alternarModal();
+video.setAttribute('src', '');
+});
+
+
+
+
